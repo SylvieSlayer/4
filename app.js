cd89677e10ef9ad0591075dd7a8e84a73235d581
SYLVIE FISHER <iwalkedwithasylvie@gmail.com>
2022-04-21 00:23:15 -0700

    AddedCheckbox

--- app.js
+++ app.js
@@ -21,3 +21,17 @@ function favcat() {
 
 }
 favcat();
+
+
+const otherCheckbox = document.querySelector('#other');
+cont otherText = document.querySelector('#otherValue');
+otherText.style.visibility = 'hidden';
+
+otherCheckbox.addEventListener('change', ) => {
+    if(otherCheckbox.checked) {
+        otherText.style.visibility = 'visible';
+        otherText.value = '';
+    } else {
+        otherText.style.visibility = 'hidden';
+    }
+});
